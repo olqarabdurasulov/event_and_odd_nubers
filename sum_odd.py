b91e9e03dd73ed74081840dab93ec56d4bd662ba
olqarabdurasulov <abdurasulovolqar123@gmail.com>
2022-12-01 22:19:21 +0500

    toq sonlar yig`indisi

--- sum_odd.py
+++ sum_odd.py
@@ -3,10 +3,10 @@
 #Create a variable "var_int" and assign it a four-digit integer value.
 var_int = 5832
 #Create a variable "sum_even" and assign it 0.
-sum_even = 0
+sum_odd = 0
 #Find the sum of the odd digits in the variable "var_int".
 a = var_int%10
 b = var_int//10%10
 c = var_int//100%10
 d = var_int//1000
-sum_even = ((a)%2 + (b)%2 + (c)%2 + (d)%2)
+sum_odd = ((a)%2 + (b)%2 + (c)%2 + (d)%2)
